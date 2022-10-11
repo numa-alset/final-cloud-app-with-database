eb7bcfb033847b4e9ac2301a21309257ae6fe8b3
numa-alset <111363953+numa-alset@users.noreply.github.com>
2022-10-11 23:15:30 +0300

    Update views.py

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -111,37 +111,27 @@ def enroll(request, course_id):
          # Add each selected choice object to the submission object
          # Redirect to show_exam_result with the submission id
 def submit(request, course_id):
-    request.objects.get(user=request.user, course=course_id)
+    Enrollment.objects.get(user=request.user, course=course_id)
+    Submission.objects.create(enrollment=Enrollment.objects.get(Enrollment.id))
+    extract_answers(request)
+    submitted_anwsers = extract_answers(request)
+    for sub in submitted_anwsers:
+        Submission.choices=sub
+    return redirect('show_exam_result', Submission.id)
 
+# <HINT> A example method to collect the selected choices from the exam form from the request object
+def extract_answers(request):
     submitted_anwsers = []
     for key in request.POST:
         if key.startswith('choice'):
             value = request.POST[key]
             choice_id = int(value)
             submitted_anwsers.append(choice_id)
-    
- 
-    Submission.objects.create(enrollment=course,choices=submitted_anwsers)
-    return redirect('show_exam_result')
+    return submitted_anwsers
 
-# <HINT> A example method to collect the selected choices from the exam form from the request object
-#def extract_answers(request):
-#    submitted_anwsers = []
-#    for key in request.POST:
-#        if key.startswith('choice'):
-#            value = request.POST[key]
-#            choice_id = int(value)
-#            submitted_anwsers.append(choice_id)
-#    return submitted_anwsers
-
-
-# <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question,
-# you may implement it based on the following logic:
-        # Get course and submission based on their ids
-        # Get the selected choice ids from the submission record
-        # For each selected choice, check if it is a correct answer or not
-        # Calculate the total score
-def show_exam_result(request):
+def show_exam_result(request,course_id, submission_id):
+    Course.id=Course_id
+    Submission.id=submission_id
     selected_choice = Submission.choices
     win=0
     num=0
@@ -150,6 +140,14 @@ def show_exam_result(request):
             win +=1
         num +=1
     grade =(win/num)*100
-    return render(request, 'exam_result_bootstrap.html', {"course_id" : course_id,"grade" : grade})
+    return render(request, 'exam_result_bootstrap.html', {"course.id" : course.id,"selected_choice" : Submission.choices,"grade" : grade})
 
+
+
+# <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question,
+# you may implement it based on the following logic:
+        # Get course and submission based on their ids
+        # Get the selected choice ids from the submission record
+        # For each selected choice, check if it is a correct answer or not
+        # Calculate the total score
 
